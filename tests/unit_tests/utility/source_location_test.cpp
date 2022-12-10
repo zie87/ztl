4a058366f7e068e32012faefe676168f0416e99a
Tobias Zindl <zie.develop@gmail.com>
2022-12-10 13:21:47 +0100

    [utility]: add source location

---
+++ tests/unit_tests/utility/source_location_test.cpp
@@ -0,0 +1,22 @@
+#include "ztl/utility/source_location.hpp"
+
+#include "test_framework.hpp"
+
+#include <cstring>
+
+namespace {
+
+auto test_function() noexcept -> ztl::source_location {
+    const auto loc = ztl::source_location::current();
+    return loc;
+}
+
+}  // namespace
+
+TEST(utility_source_location) {
+    const auto loc = test_function();
+
+    UT_CHECK_EQUAL(loc.line(), 10);
+    UT_CHECK_EQUAL(std::strcmp(loc.function_name(), "test_function"), 0);
+    UT_CHECK_EQUAL(std::strcmp(loc.file_name(), __FILE__), 0);
+}
